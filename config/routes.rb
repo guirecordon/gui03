57c1953aa59cd08b671c3a6d48ae3d4153fb5d85
Gui Recordon <guilherme.recordon@gmail.com>
2022-12-20 17:34:58 -0300

    Add devise gem and setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/profile'
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get '/u/:id', to: 'users#profile', as: 'user'
+
   resources :posts
   get 'about', to: "pages#about"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+User.create(email: 'user01@email.com', password: 'password', password_confirmation: 'password')
+
 10.times do |x|
-  Post.create(title: "Title #{x + 1}", body: "Body #{x + 1} this is my body")
+  Post.create(title: "Title #{x + 1}", body: "Body #{x + 1} this is my body", user_id: User.first.id)
 end
